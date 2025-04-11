755b1adbee439c2c3582748cc8703c32946667e5
H.A <ha@noreply.github.com>
2025-04-10 18:53:29 -0700

    Clean up old Brewfiles, track new ~/.config/homebrew/Brewfile

--- .config/zsh/modules/14-homebrew.zsh
+++ .config/zsh/modules/14-homebrew.zsh
@@ -2,4 +2,4 @@
 # Homebrew
 # =============================================================================
 alias brewup='brew update; brew upgrade; brew cleanup'
-alias brewupdate="brew bundle dump --file=~/.Brewfile --force && yadm add ~/.Brewfile && yadm commit -m \"Updated Brewfile\" && yadm push"
+alias brewupdate="brew bundle dump --file=$HOME/.config/homebrew/Brewfile --force && yadm add $HOME/.config/homebrew/Brewfile && yadm commit -m \"Updated Brewfile\" --no-gpg-sign && yadm push"
