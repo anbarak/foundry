d7ea8784a5a92da83dba91798a7f4a53b6744391
H.A <ha@noreply.github.com>
2025-04-12 04:58:03 -0700

    Automate dev environment creation

--- .config/zsh/modules/01-coreutils.zsh
+++ .config/zsh/modules/01-coreutils.zsh
@@ -80,3 +80,4 @@ alias rg='rg --hidden --no-ignore'  # safe to use alongside ggrep
 # Use to lint dotfile before yadm commit
 alias lintdot='"$HOME/bin/lint-dotfiles"'
 alias ycommit='"$HOME/bin/yadm-commit"'
+alias devrestore='"$HOME/bin/restore-dev-env"'
